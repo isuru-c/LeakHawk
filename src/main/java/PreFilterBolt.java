b0661d609d9652c862c95b6495cab344b6d6fced
sugeesh <lpdneo@gmail.com>
2017-07-31 21:50:44 +0530

    Added Prefilter functionalities

--- src/main/java/PreFilterBolt.java
+++ src/main/java/PreFilterBolt.java
@@ -6,6 +6,11 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -21,7 +26,6 @@ public class PreFilterBolt extends BaseRichBolt {
     }
 
     public void execute(Tuple tuple) {
-
         String type = tuple.getString(0);
         String key = tuple.getString(1);
         String date = tuple.getString(2);
@@ -30,12 +34,33 @@ public class PreFilterBolt extends BaseRichBolt {
         String syntax = tuple.getString(5);
         String post = tuple.getString(6);
 
-        //System.out.println("\nKey: " + key + "\nDate: " + date + "\nUser: " + user + "\nTitle: " + title + "\n" + post + "\nCount: " + count++);
 
-        collector.emit(tuple, new Values(type, key, date, user, title, syntax, post));
+        //System.out.println("\nKey: " + key + "\nDate: " + date + "\nUser: " + user + "\nTitle: " + title + "\n" + post + "\nCount: " + count++);
+        if(!isContainKeyWord(post)) {
+            collector.emit(tuple, new Values(type, key, date, user, title, syntax, post));
+        }
 
         collector.ack(tuple);
 
+    }
+
+    private boolean isContainKeyWord(String post) {
+        ArrayList keyWordList = new ArrayList<String>();
+        keyWordList.add("Game");
+        keyWordList.add("Sports");
+
+        try {
+
+            for (int i=0;i<keyWordList.size();i++) {
+                if (post.toUpperCase().contains(keyWordList.get(i).toString().toUpperCase())) {
+                    //exit after the first successful hit
+                    return true;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return false;
     }
 
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
